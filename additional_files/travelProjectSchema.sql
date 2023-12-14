35dced40493c82f878403c39399a582c993c5f85
Dany Chucri <125823060+Dany-Chucri@users.noreply.github.com>
2023-12-13 21:12:31 -0500

    Update travelProjectSchema.sql
    Removed comments

--- additional_files/travelProjectSchema.sql
+++ additional_files/travelProjectSchema.sql
@@ -33,7 +33,6 @@ CREATE TABLE IF NOT EXISTS QuestionsAndAnswers(qID int auto_increment primary ke
 INSERT INTO AirlineCompany values ('AB'), ('CD'), ('EF'), ('GH'), ('IJ'), ('KL');
 INSERT INTO Airport values ('JFK'), ('NJA'), ('EWR'), ('SYD'), ('PEK'), ('HNL');
 INSERT INTO Customer values ('Timmy', 'Chen', 'txc', ''), ('Spencer', 'Tong', 'sxt', ''), ('Dany', 'Chucri', 'dxc', ''), ('Alex', 'Walcsak', 'asw', '');
--- INSERT INTO TicketReserves values (3, 2, 33.0, 70.0, '2025-03-04 10:25:21', 'sxt', 20) , (1, 1, 20.0, 40.0, '2023-12-31 12:30:00', 'sxt', 0), (2, 2, 30.0, 50.0, '2020-11-29 10:25:21', 'asw', 20);
 INSERT INTO AssociatedWith values('JFK', 'AB'), ('NJA','CD'), ('EWR', 'EF'), ('SYD', 'GH'), ('PEK', 'IJ'), ('HNL', 'KL');
 INSERT INTO Aircraft values ('aircraft1', 'MTWRFSU', 2, 'AB'), ('aircraft2', 'MTWRFSU', 95, 'AB'), ('aircraft3', 'MTWRFSU', 50, 'EF'), ('aircraft4', 'MTWRFSU', 20, 'GH'), ('aircraft5', 'MTWRFSU', 500, 'IJ');
 INSERT INTO FlightAssignedTo values (123, '2024-01-20', '2024-02-01', null, null, 'JFK', 'NJA', 0, 'o', 50.00, 75.00, 20.00, 'AB', 'aircraft1', ''), 
@@ -43,6 +42,5 @@ INSERT INTO FlightAssignedTo values (123, '2024-01-20', '2024-02-01', null, null
 (102, '2024-02-01 02:00:00', '2023-02-01 07:23:00', '2023-02-05 19:30:00', '2024-02-05 23:30:00', 'HNL', 'PEK', '0', 'r', 1300.23, 1900.78, 35.00, 'IJ', 'aircraft5', ''),
 (103, '2025-08-10 10:00:00', '2025-08-10 15:30:00', '2025-08-20 14:00:00', '2025-08-20 20:30:00', 'JFK', 'HNL', '1', 'r', 200.00, 300.36, 35.00, 'GH', 'aircraft4', '');
 
--- INSERT INTO TicketHasFlight values (1, 123, 'AB'), (2, 456, 'CD');
 INSERT INTO AssociatedAccount values ('timmyMen', 'password1', 'portfolio1', 'txc'), ('danychurro', 'password2', 'portfolio2', 'dxc');
 INSERT INTO QuestionsAndAnswers values (100, 'content', 'answer!'), (105, 'Can I cancel flights for free?', 'It depends on your ticket type.!');
